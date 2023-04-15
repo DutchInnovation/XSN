fc91821b1ac4ed24f15ce8d60286a833745e2b49
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-04-15 17:40:45 +0900

    login and signup static pages

--- areas/register/views/index.php
+++ areas/register/views/index.php
@@ -1,45 +1,63 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Sign Up</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-  <header>
-        <div class="logo">
-          <a href="index.php">
-            <img src="logo.png" alt="Logo"  height="176">
-          </a>
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#">Store</a></li>
-            <li><a href="#">Community</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Support</a></li>
-          </ul>
-        </nav>
-        <div>
-            <a href="login.php">login</a> | language
+<?php
+import("sprint/partials/plain_nav.php");
+?>
+
+<div class="w-100" style="display:flex;justify-content:center;align-items:center;height:calc(100% - 68px)">
+
+  <div class="card" style="width:100%;max-width:800px">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-12">
+          <h5 class="card-title mb-0">Create an account</h5>
+          <form class="mt-4" action="login" method="post">
+            <div class="form-group">
+              <div class="textfield-box">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username">
+              </div>
             </div>
-      </header>
-    <h1>Sign Up</h1>
-    <form>
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
+            <div class="form-group">
+              <div class="textfield-box">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="textfield-box">
+                <label for="password">Confirm Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+              </div>
+            </div>
+            <div class="form-group mb-4">
+              <div class="textfield-box">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+              </div>
+            </div>
+            <hr>
+            <div class="form-group mt-4">
+              <div class="textfield-box">
+                <label for="security_question">Security Question</label>
+                <select class="form-control" id="security_question" name="security_question">
+                  <option value="1">What is your mother's maiden name?</option>
+                  <option value="2">What is your favorite color?</option>
+                  <option value="3">What is your favorite food?</option>
+                  <option value="4">What is your favorite movie?</option>
+                  <option value="5">What is your favorite book?</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="textfield-box">
+                <label for="security_answer">Security Answer</label>
+                <input type="text" class="form-control" id="security_answer" name="security_answer">
+              </div>
+            </div>
 
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
+            <button type="submit" class="btn btn-secondary mt-3">Complete sign up</button>
+          </form>
+        </div>
+      </div>
+    </div>
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-
-      <label for="confirm_password">Confirm Password:</label>
-      <input type="password" id="confirm_password" name="confirm_password" required>
-
-      <button type="submit">Sign Up</button>
-    </form>
-  </body>
-</html>
+</div>
---
+++ sprint/partials/plain_nav.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light bg-light p-3">
+  <a class="navbar-brand" href=".">
+    <img src="assets/img/logo.png" width="50" class="d-inline-block align-top">
+    XSN
+  </a>
+</nav>
